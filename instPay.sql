61b8b1ae2dfb472db74e13e794161bf486cdcfe5
mdarwinj382000 <mdarwinj@gmail.com>
2022-07-13 09:29:29 +0530

    updates sql

--- instPay.sql
+++ instPay.sql
@@ -2,7 +2,7 @@
 drop schema
 
 if exists instpay;
-     CREATE SCHEMA instpay COLLATE = utf8_general_ci;
+     CREATE SCHEMA instpay;
 
 use instpay;
 
@@ -15,7 +15,6 @@ create Table Admin(
       first_name varchar(30) not null,
       last_name varchar(30) not null,
       email varchar(35) not null,
-      password varchar(30) not null,
       primary key(admin_id)
 );
 
@@ -24,43 +23,32 @@ create table accountant(
       first_name varchar(30) not null,
       last_name varchar(30) not null,
       email varchar(35) not null,
-      password varchar(30) not null,
-      branch_id int not null,
+      branch_name varchar(40) not null,
       primary key(accountant_id)
 );
 
 create table student(
-	  student_id char(20) not null,
+	  enroll_no char(20) not null,
       first_name varchar(30) not null,
       last_name varchar(30) not null,
       email varchar(35) not null,
       password varchar(30) not null,
-      father_name varchar(30) not null,
       phone_number VARCHAR(20) not null,
       current_semester int not null,
-      branch_id int not null,
-      primary key(student_id)
+      branch_name varchar(40) not null,
+      primary key(enroll_no)
 );
 
 create table payment(
       payment_id int not null auto_increment,
-	  student_id char(20) not null,
+	  enroll_no char(20) not null,
       semester_no int not null,
-      amount_payed int not null,
+      remain_amnt int not null,
       total_amount int not null,
       primary key(payment_id)
 );
 
-create table branch_fee(
-	branch_id int not null auto_increment,
-    branch_name varchar(30) not null,
-    semester_no int not null,
-    fee_payment int not null,
-    primary key(branch_id)
-);
-
-alter table accountant add foreign key(branch_id) references branch_fee(branch_id);
-alter table student add foreign key(branch_id) references branch_fee(branch_id);
+alter table payment add foreign key(enroll_no) references student(enroll_no);
 
 
 
